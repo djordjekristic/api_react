2222d3c0eb2fc867e0b87078d53ea61303e3cc78
djordjekristic <strelac.989@gmail.com>
2024-08-30 01:05:41 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//https://www.omdbapi.com/?t=Terminator&apikey=7cbb12f4  primer API poziva
+
+const App = () =>{
+   return(
+    <>
+    </>
+   )
 }
-
-export default App;
+export default App
