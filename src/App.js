f6f877670f14330144d8eaa9c5e2539f53544f1b
djordjekristic <strelac.989@gmail.com>
2024-08-30 23:49:54 +0200

    add env file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import axios from "axios"
 
+
+ 
 //https://www.omdbapi.com/?t=Terminator&apikey=7cbb12f4  primer API poziva
+
+
+//fetch("https://www.omdbapi.com/?t=Terminator&apikey=7cbb12f4").
+//then(response => response.json()).
+//then(JsonResponse => console.log(JsonResponse))
+
+console.log(process.env.REACT_APP_OMDBAPI_KEY);
+
+axios.get("https://www.omdbapi.com/?t=Terminator&apikey=7cbb12f4")
+.then(response => console.log(response.data))
+.catch(error => console.log(error) )
+  
+
+
+
+
 
 const App = () =>{
    return(
     <>
+    <p>test</p>
     </>
    )
 }
+
 export default App
+
